a6aaa66c1f36121a050d50d3f1b77bd0f55e73f0
Bedlu Erpassa <bedluerpassa@Bedlus-MBP.home>
2021-02-22 16:42:16 -0500

    hekek

---
+++ src/main/java/com/HospitalMangagmentSystem/demo/controller/RoleController.java
@@ -0,0 +1,37 @@
+package com.HospitalMangagmentSystem.demo.controller;
+
+import com.HospitalMangagmentSystem.demo.Dto.PatientDto;
+import com.HospitalMangagmentSystem.demo.Service.PatientService;
+import com.HospitalMangagmentSystem.demo.domain.Patients;
+import com.HospitalMangagmentSystem.demo.domain.Role;
+import com.HospitalMangagmentSystem.demo.domain.Rolename;
+import com.HospitalMangagmentSystem.demo.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.transaction.Transactional;
+import java.net.URI;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class RoleController {
+    @Autowired
+    RoleRepository roalrepository;
+    @PostMapping("/role")
+    @Transactional
+    public void createrole() {
+        Role role = new Role();
+        role.setName(Rolename.ROLE_USER);
+        roalrepository.save(role);
+        Role role2 = new Role();
+        role2.setName(Rolename.ROLE_DOCTOR);
+        roalrepository.save(role2);
+
+
+
+
+    }
+}
